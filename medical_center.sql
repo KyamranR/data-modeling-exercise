9a9713ab53bc25fa501d0cf87fdcc166dc4fef9f
Kyamran Rizayev <kyamranr@gmail.com>
2024-02-26 15:45:31 -0600

    finished medical center exercises

--- medical_center.sql
+++ medical_center.sql
@@ -1,2 +1,32 @@
 
 
+CREATE DATABASE medical_center;
+
+\c medical_center
+
+CREATE TABLE doctors
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE patients
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INT NOT NULL
+);
+
+CREATE TABLE diseases
+(
+    id SERIAL PRIMARY KEY,
+    disease_name TEXT NOT NULL
+);
+
+CREATE TABLE patient_visits
+(
+    id SERIAL PRIMARY KEY,
+    doctor_id INT REFERENCES doctors(id),
+    patient_id INT REFERENCES patients(id),
+    disease_id INT REFERENCES diseases(id)
+);
