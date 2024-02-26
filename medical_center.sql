17eaf9d15036ec31e5a06f1368c77882d85fcdc8
Kyamran Rizayev <kyamranr@gmail.com>
2024-02-26 16:14:29 -0600

    inserted some info into the tables

--- medical_center.sql
+++ medical_center.sql
@@ -30,3 +30,27 @@ CREATE TABLE patient_visits
     patient_id INT REFERENCES patients(id),
     disease_id INT REFERENCES diseases(id)
 );
+
+INSERT INTO doctors (name)
+VALUES ('Dr. DoLittle');
+
+INSERT INTO doctors (name)
+VALUES ('Dr. DoAlot');
+
+INSERT INTO patients (name, age)
+VALUES ('Tiger', 25);
+
+INSERT INTO patients (name, age)
+VALUES ('Monkey', 50);
+
+INSERT INTO diseases (disease_name)
+VALUES ('Heart Diseases');
+
+INSERT INTO diseases (disease_name)
+VALUES ('Kidney Diseases');
+
+INSERT INTO patient_visits (doctor_id, patient_id, disease_id)
+VALUES (1, 1, 1); -- Dr. DoLittle sees Tiger for Heart Diseases
+
+INSERT INTO patient_visits (doctor_id, patient_id, disease_id)
+VALUES (2, 2, 2); -- Dr. DoAlot sees Monkey for Kidney Diseases
