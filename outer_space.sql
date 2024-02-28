ee7d94bc88d5b2b5d624f44cda626fd4ec69173e
Kyamran Rizayev <kyamranr@gmail.com>
2024-02-28 10:45:25 -0500

    finished outer space, improved it

--- outer_space.sql
+++ outer_space.sql
@@ -7,22 +7,40 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
+CREATE TABLE galaxies
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  planet_id INT REFERENCES planets(id)
+);
+
 CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
   orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy_id INT REFERENCES galaxies(id)
 );
 
+INSERT INTO galaxies (name) VALUES ('Milky Way');
+
 INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+  (name, orbital_period_in_years, orbits_around, galaxy_id)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Earth', 1.00, 'The Sun', 1),
+  ('Mars', 1.88, 'The Sun', 1),
+  ('Venus', 0.62, 'The Sun', 1),
+  ('Neptune', 164.8, 'The Sun', 1),
+  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 1),
+  ('Gliese 876 b', 0.23, 'Gliese 876', 1);
+
+
+  INSERT INTO moons (name, planet_id) 
+  VALUES ('The Moon', 1), ('Phobos', 2), ('Deimos', 2);
