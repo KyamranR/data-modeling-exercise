96f8c06c57bdc03562fb5494a81a34d1668f36e4
Kyamran Rizayev <kyamranr@gmail.com>
2024-02-28 20:19:18 -0600

    finished music exercises did improvements and inserted info

--- music.sql
+++ music.sql
@@ -7,27 +7,66 @@ CREATE DATABASE music;
 
 \c music
 
+CREATE TABLE artists
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE albums
+(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  release_date DATE NOT NULL,
+  producer_id INT REFERENCES producers(id)
+);
+
+CREATE TABLE producers
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
 CREATE TABLE songs
 (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
   release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  album_id INT REFERENCES albums(id)
 );
 
-INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
-VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+CREATE TABLE songs_artists
+(
+  song_id INT REFERENCES songs(id),
+  artist_id INT REFERENCES artists(id),
+  PRIMARY KEY (song_id, artist_id)
+);
+
+INSERT INTO artists (name) 
+VALUES ('Hanson'), ('Queen'), ('Mariah Carey'), 
+('Boyz II Men'), ('Lady Gaga'), ('Bradley Cooper'), 
+('Nickelback'), ('Jay Z'), ('Alicia Keys'), 
+('Katy Perry'), ('Juicy J'), ('Maroon 5'), 
+('Christina Aguilera'), ('Avril Lavigne'), ('Destiny''s Child');
+
+
+INSERT INTO albums (title, release_date, producer_id) 
+VALUES ('Middle of Nowhere', '1997-04-15', 1), 
+('A Night at the Opera', '1975-10-31', 2), 
+('Daydream', '1995-11-14', 3), ('A Star Is Born', '2018-09-27', 4), 
+('Silver Side Up', '2001-08-21', 5), ('The Blueprint 3', '2009-10-20', 6), 
+('Prism', '2013-12-17', 7), ('Hands All Over', '2011-06-21', 8), 
+('Let Go', '2002-05-14', 9), ('The Writing''s on the Wall', '1999-11-07', 10);
+
+INSERT INTO songs (title, duration_in_seconds, release_date, album_id) 
+VALUES ('MMMBop', 238, '1997-04-15', 1), ('Bohemian Rhapsody', 355, '1975-10-31', 2), 
+('One Sweet Day', 282, '1995-11-14', 3), ('Shallow', 216, '2018-09-27', 4), 
+('How You Remind Me', 223, '2001-08-21', 5), 
+('New York State of Mind', 276, '2009-10-20', 6), 
+('Dark Horse', 215, '2013-12-17', 7), ('Moves Like Jagger', 201, '2011-06-21', 8), 
+('Complicated', 244, '2002-05-14', 9), ('Say My Name', 240, '1999-11-07', 10);
+
+INSERT INTO songs_artists (song_id, artist_id) 
+VALUES (1, 1), (2, 2), (3, 3), (3, 4), (4, 5), (4, 6), (5, 7), (6, 8), 
+(6, 9), (7, 10), (7, 11), (8, 12), (8, 13), (9, 14), (10, 15);
